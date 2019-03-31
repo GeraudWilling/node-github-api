3e8fe050c7afe7db08634d38d9432bd74d80e541
GéraudWilling <geraudwilling@hotmail.fr>
2019-03-31 06:19:57 +0200

    Adding timer

--- main.js
+++ main.js
@@ -15,30 +15,33 @@ var Configs = JSON.parse(contents);
 
 Configs.repos.forEach(repository => {
     console.log(repository);
-    clientWithAuth.repos.get({
-        owner: 'GeraudWilling',
-        repo: repository
-    }).then(({ data, status, headers }) => {
-        //console.log(data);
-        //Send data to dashboard widget
-        const options = {
-            method: 'POST',
-            url: process.env.GUI_URL+'/widgets/' +repository,
-            body: {
-                "auth_token": 'YOUR_AUTH_TOKEN',
-                "repo": data.full_name,
-                "issues": data.open_issues_count,
-                "forks": data.forks,
-                "watchers": data.subscribers_count,
-                "stargazers": data.stargazers_count,
-                "activity": data.updated_at
-            },
-            json: true  // JSON stringifies the body automatically
-        }
-        postToWidget(options);
-    }).catch(function(error) {
-        console.error(error);
-    });
+    setInterval( () => {  //Scheduler Timer
+        clientWithAuth.repos.get({
+            owner: 'GeraudWilling',
+            repo: repository
+        }).then(({ data, status, headers }) => {
+            //console.log(data);
+            //Send data to dashboard widget
+            const options = {
+                method: 'POST',
+                url: process.env.GUI_URL+'/widgets/' +repository,
+                body: {
+                    "auth_token": 'YOUR_AUTH_TOKEN',
+                    "repo": data.full_name,
+                    "issues": data.open_issues_count,
+                    "forks": data.forks,
+                    "watchers": data.subscribers_count,
+                    "stargazers": data.stargazers_count,
+                    "activity": data.updated_at
+                },
+                json: true  // JSON stringifies the body automatically
+            }
+            postToWidget(options);
+        }).catch(function(error) {
+            console.error(error);
+        });
+    }, 60*1000); 
+    
 });
 
 
